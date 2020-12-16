3b654fb128699c7dcda8fca714e3bbd2b390ad2e
Javortper <javiortiz110@gmail.com>
2020-12-16 16:35:10 +0000

    GET con parametros

--- server.js
+++ server.js
@@ -11,47 +11,6 @@ app.get("/", (req, res) => {
     res.send("<html><body><h1>My server</h1></body></html>");
 });
 
-// GET viajes
-app.get(BASE_API_PATH + "/travels", (req, res) => {
-    console.log(Date() + " - GET /travels");
-    Travel.find({}, (err, travels) => {
-        if (err) {
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
-        } else {
-            res.send(travels.map((travel) => {
-                return travel;
-            }));
-        }
-    });
-});
-
-// GET viajes por ID
-app.get(BASE_API_PATH + "/travels/:id", (req, res) => {
-    console.log(Date() + " - GET /travels");
-    Travel.findById(req.params.id, (err, travels) => {
-        if (err) {
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
-        } else {
-            res.send(travels)
-        }
-    });
-});
-
-// GET viaje por ID de cliente
-app.get(BASE_API_PATH + "/travels/cliente/:id", (req, res) => {
-    console.log(Date() + " - GET /travels");
-    Travel.find({id_cliente:req.params.id}, (err, travels) => {
-        if (err) {
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
-        } else {
-            res.send(travels)
-        }
-    });
-});
-
 // POST crear viaje
 app.post(BASE_API_PATH + "/travels", (req, res) => {
     console.log(Date() + " - POST /travels");
@@ -66,7 +25,33 @@ app.post(BASE_API_PATH + "/travels", (req, res) => {
     });
 });
 
+// GET con busqueda
+app.get(BASE_API_PATH + "/travels/find", (req, res) => {
+    console.log(Date() + " - GET /travels");
+    Travel.find(req.query, (err, travels) => {
+        if (err) {
+            console.log(Date() + "-" + err);
+            res.sendStatus(500);
+        } else{
+            console.log(req.query);
+            res.send(travels)
+        }
+    });
+});
 
+
+// // GET viajes por ID
+// app.get(BASE_API_PATH + "/travels/:id", (req, res) => {
+//     console.log(Date() + " - GET /travels");
+//     Travel.findById(req.params.id, (err, travels) => {
+//         if (err) {
+//             console.log(Date() + "-" + err);
+//             res.sendStatus(500);
+//         } else {
+//             res.send(travels)
+//         }
+//     });
+// });
 
 
 module.exports = app;
