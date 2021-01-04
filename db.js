cd806159c73bbeea9ac86257039fd7088bfe93a1
javortper <javiortiz110@gmail.com>
2021-01-04 21:01:29 +0100

    Travis tests

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var DataStore = require('mongoose');
 var mongoose = require('mongoose');
 
-const DB_URL = ('mongodb+srv://admin:admin@cluster0.upzcp.mongodb.net/test' || 'mongodb://localhost/');
+const DB_URL = ('mongodb+srv://admin:admin@cluster0.upzcp.mongodb.net/test' || 'mongodb://mongo/test');
 
 const dbConnect = function() {
     const db = mongoose.connection;
