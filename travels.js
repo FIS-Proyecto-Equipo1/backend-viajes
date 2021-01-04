65399b90b0004db49e182eb8117dd25f8cb671e1
javortper <javiortiz110@gmail.com>
2021-01-04 20:41:52 +0100

    Travis tests

--- travels.js
+++ travels.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const valores_estado = ['EN CURSO', 'FINALIZADO']
-const format_duracion=new RegExp('[0-9]{3}[ ]{1}[min]{1}'); //TODO: NO FUNCIONA, CAMBIAR
+const format_duracion=new RegExp('[0-9]{3}[ ]{1}[min]{1}'); //TODO: NO FUNCIONA, CAMBIAR y AÑADIR en duracion
 
 
 const travelSchema = new mongoose.Schema({
     id_cliente: {type:String, unique: true, required: true},
     id_vehiculo: {type:String, unique: true, required: true}, //TODO: MATRICULA?
     estado: {type: String, required: true, enum:valores_estado},
-    duracion: {type: String, required: true, match:format_duracion}
+    duracion: {type: String, required: true}
 });
 
 
