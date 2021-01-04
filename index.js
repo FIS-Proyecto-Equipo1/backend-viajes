7b13a2356520da3046626e0e1250b24508f29e94
javortper <javiortiz110@gmail.com>
2021-01-04 20:38:25 +0100

    Travis runs test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server.js');
 const dbConnect = require('./db');
 
-var port = (process.env.PORT || 4000); //CAMBIADO PARA HEROKU
+var port = (process.env.PORT || 'mongodb://mongo/test'); //CAMBIADO PARA HEROKU
 
 console.log("Starting API server at "+port);
 
