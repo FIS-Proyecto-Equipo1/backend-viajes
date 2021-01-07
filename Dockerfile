a785d771da0fa3044d51eef0b9e663d85afda6d1
jossanlop <jose.enrique.sanchez@outlook.com>
2021-01-07 15:47:22 +0000

    pull travis

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,15 @@ COPY package-lock.json .
 RUN npm install
 
 COPY index.js .
+<<<<<<< HEAD
 COPY db.js .
 COPY server.js .
 COPY travels.js .
+=======
+COPY travels.js .
+COPY db.js .
+COPY server.js .
+>>>>>>> 8a5714d43f9be361364f1ae6651537e3357a1ff2
 
 EXPOSE 4000
 
--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var DataStore = require('mongoose');
 var mongoose = require('mongoose');
 
-const DB_URL = (process.env.MONGO_URL || 'mongodb://localhost/');
+const DB_URL = ('mongodb+srv://admin:admin@cluster0.upzcp.mongodb.net/test' || 'mongodb://localhost/');
 
 const dbConnect = function() {
     const db = mongoose.connection;
